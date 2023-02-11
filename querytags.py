42c8a933d8260045b5314ab17fc6351efff515e6
Full Aware <fullaware@users.noreply.github.com>
2023-02-10 23:10:31 -0500

    lookup works

--- querytags.py
+++ querytags.py
@@ -1,20 +1,27 @@
 from dbconn import db
 import pprint
 
-cursor = db.products.find({"tags":  {"$all": ["63e54024bf10b7a958ae37eb"] }})
+""" cursor = db.products.find({"tags":  {"$all": ["63e54024bf10b7a958ae37eb"] }})
 
 for doc in cursor:
-    pprint.pprint(doc)
+    pprint.pprint(doc) """
 
 
 cursor2 = db.products.aggregate( [
    {
+        "$unwind":"$tags"
+   },{
       "$lookup":
          {
-            from: "tags",
-            localField: "tags",
-            foreignField: "name",
-            as: "tagAssets"
+            "from": "tags",
+            "localField": "tags",
+            "foreignField": "_id",
+            "as": "tagAssets"
         }
+   },{
+    "$project":{"_id":0,"tagAssoc":"$tagAssets.tagAssoc","tagLabel":"$tagAssets.tagLabel"}
    }
 ] )
+
+for doc2 in cursor2:
+    pprint.pprint(doc2)
