c6dd3972ebb94339dd68af29bfb5a3c9b59072af
Full Aware <fullaware@users.noreply.github.com>
2023-02-11 16:34:08 -0500

    dropped unwind

--- arrays.py
+++ arrays.py
@@ -3,7 +3,7 @@ import pprint
 from bson.objectid import ObjectId
 
 
-db.products.updateOne(
+db.products.update_one(
     {"_id": ObjectId("63e54261bf10b7a958ae37f3")},
     {"$push": {"tags": {"$each": [ObjectId("63e6bbe080bd7b7c45c330e0"), ObjectId(
         "63e6bbe080bd7b7c45c330db"), ObjectId("63e6bbe080bd7b7c45c330e4")]}}}
--- querytags.py
+++ querytags.py
@@ -1,5 +1,6 @@
 from dbconn import db
 import pprint
+from bson.objectid import ObjectId
 
 """ cursor = db.products.find({"tags":  {"$all": ["63e54024bf10b7a958ae37eb"] }})
 
@@ -8,19 +9,25 @@ for doc in cursor:
 
 
 cursor2 = db.products.aggregate( [
-   {
-        "$unwind":"$tags"
-   },{
-      "$lookup":
-         {
-            "from": "tags",
-            "localField": "tags",
-            "foreignField": "_id",
-            "as": "tagAssets"
-        }
-   },{
-    "$project":{"_id":0,"tagAssoc":"$tagAssets.tagAssoc","tagLabel":"$tagAssets.tagLabel"}
-   }
+  {"$match":{"_id":ObjectId("63e54261bf10b7a958ae37f3")}},
+  {
+    "$lookup": {
+      "from": "tags",
+      "localField": "tags",
+      "foreignField": "_id",
+      "as": "tagAssets",
+    },
+  },
+  {
+    "$unwind": "$tagAssets",
+  },
+  {
+    "$project": {
+      "_id":0,
+      "tagLabel": "$tagAssets.tagLabel",
+      "tagAssoc": "$tagAssets.tagAssoc",
+    },
+  },
 ] )
 
 for doc2 in cursor2:
