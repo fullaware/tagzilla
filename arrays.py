9376623b3c496729136ce39949c1f77ef8afa7c5
Full Aware <fullaware@users.noreply.github.com>
2023-02-10 23:40:36 -0500

    arrays attempt

---
+++ arrays.py
@@ -0,0 +1,15 @@
+from dbconn import db
+import pprint
+from bson.objectid import ObjectId
+
+
+db.products.updateOne(
+    {"_id": ObjectId("63e54261bf10b7a958ae37f3")},
+    {"$push": {"tags": {"$each": [ObjectId("63e6bbe080bd7b7c45c330e0"), ObjectId(
+        "63e6bbe080bd7b7c45c330db"), ObjectId("63e6bbe080bd7b7c45c330e4")]}}}
+)
+
+cursor = db.products.find({"_id": ObjectId("63e54261bf10b7a958ae37f3")})
+
+for doc in cursor:
+    pprint.pprint(doc)
