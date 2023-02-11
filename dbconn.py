8cf941a39d9edae0fcd9cb184984bf3aa7358c07
Full Aware <fullaware@users.noreply.github.com>
2023-02-11 15:54:15 -0500

    .env connection string

--- dbconn.py
+++ dbconn.py
@@ -1,7 +1,16 @@
 import pymongo
+import urllib.parse
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+db_server = config['DB_SERVER']
+db_username = config['DB_USER']
+db_password = urllib.parse.quote_plus(config['DB_PW']) # Fix for passwords with non-alphanumeric symbols
 
 # Replace the uri string with your MongoDB deployment's connection string.
-conn_str = "mongodb://localhost/tagzilla?retryWrites=true&w=majority"
+# conn_str = "mongodb://localhost/tagzilla?retryWrites=true&w=majority"
+conn_str = f"mongodb+srv://{db_username}:{db_password}@{db_server}/test"
 
 # set a 5-second connection timeout
 client = pymongo.MongoClient(conn_str, serverSelectionTimeoutMS=5000)
